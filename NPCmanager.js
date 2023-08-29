63d2ce2a3ce0f6e7d35d3bdd5ee09f4ecb31fbaa
Dan Nikolson <Kuznetsov556677@yandex.ru>
2023-08-29 17:44:38 +0300

    add: NPCmanager

---
+++ NPCmanager.js
@@ -0,0 +1,15 @@
+// return allCells;
+
+function getNonWalls(){
+    // let allCellsArray = Object.entries(allCells);
+    for (let index = 0; index === cellTeg.className != "Wall"; index++) {
+        const element = array[index];
+        
+    }
+}
+
+NPCmanager = () =>{
+    Bat = { x: 1, y: 38 };
+    Ghost = { x: 1, y: 1 };
+    Squid = { x: 1, y: 1 };
+};
--- pacmenController.js
+++ pacmenController.js
@@ -1,80 +1,102 @@
-window.addEventListener("load", () => {
-    (function () {
-        const mapSize = { x: 40, y: 40 };
-        let currentVector = { x: 1, y: 0 };
-        let pacmenPosition = { x: 1, y: 1 };
-        let oldCell = {
-            tag: null,
-            class: null,
+
+window.addEventListener("load", pacmenController);
+var pacmenController = () => {
+    const mapSize = { x: 40, y: 40 };
+    let currentVector = { x: 1, y: 0 };
+    let pacmenPosition = { x: 1, y: 1 };
+    let oldCell = {
+        tag: null,
+        class: null,
+    };
+    let allCells;
+
+    allCells = getallCells();
+    drawPacmen();
+    
+
+    document.addEventListener(
+        "keydown",
+        (event) => {
+            const keyName = event.key;
+            const vector2 = getVector(keyName);
+
+            if (vector2.x != 0 || vector2.y != 0) {
+                updatePacmenPosition(vector2);
+                drawPacmen();
+            };
+        },
+        false,
+    );
+
+    function getVector(key) {
+        let vector2 = { x: 0, y: 0 };
+
+        switch (key) {
+            case "w":
+                vector2.y -= 1;
+                break;
+            case "s":
+                vector2.y += 1;
+                break;
+            case "d":
+                vector2.x += 1;
+                break;
+            case "a":
+                vector2.x -= 1;
+                break;
+        }
+
+        return vector2;
+    }
+
+    function foldVectors(vectorA, vectorB) {
+        const result = {
+            x: vectorA.x + vectorB.x,
+            y: vectorA.y + vectorB.y
         };
 
-        drawPacmen();
+        return result;
+    }
 
-        document.addEventListener(
-            "keydown",
-            (event) => {
-                const keyName = event.key;
-                const vector2 = getVector(keyName);
-                
-                if (vector2.x != 0 || vector2.y != 0) {
-                    updatePacmenPosition(vector2);
-                    drawPacmen();
-                };
-            },
-            false,
-        );
+    function updatePacmenPosition(vector2) {
+        const newVector = foldVectors(pacmenPosition, vector2);
 
-        function getVector(key) {
-            let vector2 = { x: 0, y: 0 };
+        if (newVector.x > mapSize.x && newVector.y > mapSize.y) return pacmenPosition;
+        else if (newVector.x < 0 && newVector.y < 0) return pacmenPosition;
 
-            switch (key) {
-                case "w":
-                    vector2.y -= 1;
-                    break;
-                case "s":
-                    vector2.y += 1;
-                    break;
-                case "d":
-                    vector2.x += 1;
-                    break;
-                case "a":
-                    vector2.x -= 1;
-                    break;
-            }
+        let cellTeg = document.getElementById(`${newVector.x}-${newVector.y}`);
 
-            return vector2;
+        if (cellTeg.className != "Wall") {
+            pacmenPosition = newVector;
+            currentVector = vector2;
         }
+    }
 
-        function foldVectors(vectorA, vectorB) {
-            const result = {
-                x: vectorA.x + vectorB.x,
-                y: vectorA.y + vectorB.y
+    function drawPacmen() {
+        if (oldCell.class) oldCell.tag.className = oldCell.class;
+
+        oldCell.tag = document.getElementById(`${pacmenPosition.x}-${pacmenPosition.y}`);
+        oldCell.class = oldCell.tag.className;
+
+        oldCell.tag.className = `Pacman${currentVector.x}-${currentVector.y}`;
+    }
+
+    function getallCells() {
+        let allCells = {};
+
+        for (let x = 0; x < mapSize.x; x++) {
+            for (let y = 0; y < mapSize.y; y++) {
+                stringPosition = `${x}-${y}`;
+                allCells[stringPosition] = document.getElementById(stringPosition);
             };
+        };
+        return allCells;
+    }
 
-            return result;
-        }
+    // function a(allCells) {
+    //     // let allCellsArray = Object.entries(allCells);
+    //     console.log('ывфагнтпыгфавшпгфыгвпагшыаыафыавы');
+    // }
+    
+}
 
-        function updatePacmenPosition(vector2) {
-            const newVector = foldVectors(pacmenPosition, vector2);
-
-            if (newVector.x > mapSize.x && newVector.y > mapSize.y) return pacmenPosition;
-            else if (newVector.x < 0 && newVector.y < 0) return pacmenPosition;
-
-            let cellTeg = document.getElementById(`${newVector.x}-${newVector.y}`);
-
-            if (cellTeg.className != "Wall") {
-                pacmenPosition = newVector;
-                currentVector = vector2;
-            }
-        }
-
-        function drawPacmen() {
-            if (oldCell.class) oldCell.tag.className = oldCell.class;
-
-            oldCell.tag = document.getElementById(`${pacmenPosition.x}-${pacmenPosition.y}`);
-            oldCell.class = oldCell.tag.className;
-
-            oldCell.tag.className = `Pacman${currentVector.x}-${currentVector.y}`;
-        }
-    })();
-});
