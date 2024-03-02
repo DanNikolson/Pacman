2e3309640e01bcf1f2889f826096acef066649e0
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-03-02 18:39:38 +0300

    class ghost, ghost assets

---
+++ ghosts.js
@@ -0,0 +1,38 @@
+class Ghost {
+    constructor({ position, velocity }) {
+        this.position = {
+            x: position.x * SIZE + 5,
+            y: position.y * SIZE + 5
+        };
+        this.velocity = velocity;
+        this.width = SIZE - 10;
+        this.height = SIZE - 10;
+        this.polygon = createPolygon(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    draw(context, img) {
+        context.drawImage(
+            img,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+    }
+
+    update(context, img) {
+        if (isCollide(player.velocity)) {
+            player.velocity = { x: 0, y: 0 };
+        }
+
+        context.clearRect(this.position.x, this.position.y, this.width, this.height);
+        this.getNextPosition();
+        this.draw(context, img);
+
+    }
+
+    getNextPosition() {
+
+    }
+}
+
